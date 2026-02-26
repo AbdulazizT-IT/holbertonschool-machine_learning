2fefdb4f7f4b5027bd98ad0801f3f532617f0522
Abdulaziz <94856585+AbdulazizT-IT@users.noreply.github.com>
2026-02-26 23:28:37 +0300

    Fix indentation in sum_total function

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -16,7 +16,7 @@ def summation_i_squared(n):
         int: The sum of squares from 1 to n.
         None: If n is not a valid positive integer.
     """
-        if not isinstance(n, int) or n < 1:
-                return None
+    if not isinstance(n, int) or n < 1:
+        return None
 
-        return n * (n + 1) * (2 * n + 1) // 6
+    return n * (n + 1) * (2 * n + 1) // 6
